6c386ec4d2a46d2d4dcc7bc24a5e36108272371b
abhisheksinghghenyar <chaudharys6396@gmail.com>
2025-02-06 00:02:49 +0530

    data_ingestion modify

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer-fault"
+MANGO_DATABASE_NAME="sensor_data"
+MANGO_COLLECTION_NAME="waterfault"
+
+TARGET_COLUMN="quality"
+MANGO_DB_URL="mongodb+srv://chaudharys6396:chaudharys@cluster0.w5umw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+MODEL_FILE_NAME="model"
+MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder="artifacts"
